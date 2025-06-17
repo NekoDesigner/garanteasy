04830dc87f03f6d802a97a89324fa8228c628874
Tahar Chibane <tahar.chibane@gmail.com>
2025-06-17 13:55:55 +0200

    feat(card.component): create card component basic

--- constants/Sizes.ts
+++ constants/Sizes.ts
@@ -1,5 +1,13 @@
 export const SIZES = {
+  radius: {
+    l: 18,
+    m: 16,
+    s: 14,
+    xs: 12,
+    xxs: 8,
+  },
   icon: {
+    l: 32,
     m: 24,
     s: 20,
     xs: 16,
@@ -7,6 +15,7 @@ export const SIZES = {
   padding: {
     xxs: 4,
     xs: 8,
+    s: 14,
     m: 16,
     l: 24,
     xl: 32,
