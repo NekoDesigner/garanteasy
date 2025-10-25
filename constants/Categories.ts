cdbddbaa78a9e322bd4a953270025b11aa18d03e
Tahar Chibane <tahar.chibane@gmail.com>
2025-10-25 17:51:45 +0200

    fix(Categories.ts): change category-default 1 and 2 label

--- constants/Categories.ts
+++ constants/Categories.ts
@@ -1,8 +1,8 @@
 import { IChipsProps } from "../components/ui/Chips/@types";
 
 export const CATEGORIES_BASE: IChipsProps[] = [
-  { label: 'Électroménagé', category: 'hoursehold-electricals', showIcon: true, onPress: () => {} },
-  { label: 'Petit éléctroménagé', category: 'small-electricals', showIcon: true, onPress: () => {} },
+  { label: 'Gros électroménager', category: 'hoursehold-electricals', showIcon: true, onPress: () => {} },
+  { label: 'Petit électroménager', category: 'small-electricals', showIcon: true, onPress: () => {} },
   { label: 'Bricolage', category: 'diy', showIcon: true, onPress: () => {} },
   { label: 'Jardin', category: 'garden', showIcon: true,  onPress: () => {} },
   { label: 'Mode', category: 'fashion', showIcon: true, onPress: () => {} },
